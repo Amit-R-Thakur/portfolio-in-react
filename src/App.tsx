65ca712607380c22473f59086483a02b0d4e9e39
Amit-R-Thakur <at913513@gmail.com>
2024-04-15 16:45:07 +0530

    removed extra routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,6 @@ import Me from "./pages/Me";
 import Experiences from "./pages/Experiences";
 import Projects from "./pages/Projects";
 import Techs from "./pages/Techs";
-import Authenticated from "./HOC/Authenticated";
-const textForCapture: string = `Hey there! 👋 Explore my captured memories with just a click on the Google login button.`;
-const textForJourney: string = `Hey there! 👋 Join me on my journey with just a click on the Google login button.`;
-const textForTalk: string = `Hey there! 👋 Let's chat if you want to discuss something. Just click on the Google login button to join the conversation!`;
 
 function App() {
   return (
@@ -21,35 +17,6 @@ function App() {
           <Route path="/projects" element={<Projects />} />
 
           <Route path="/tech" element={<Techs />} />
-
-          <Route
-            path="/capture"
-            element={
-              <Authenticated
-                text={textForCapture}
-                children={<h1 className="text-white">Capture</h1>}
-              />
-            }
-          />
-
-          <Route
-            path="/talks"
-            element={
-              <Authenticated
-                text={textForTalk}
-                children={<h1 className="text-white">Talks</h1>}
-              />
-            }
-          />
-          <Route
-            path="/journey"
-            element={
-              <Authenticated
-                text={textForJourney}
-                children={<h1 className="text-white">Journey</h1>}
-              />
-            }
-          />
         </Route>
       </Routes>
     </div>
